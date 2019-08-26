2bd19595b42ae5fa97ef616e51bf52a60722beed
dangolbeeker <scottb305@outlook.com>
2019-08-26 19:07:06 -0400

    router, redux, and more added

--- conjugator/components/login.jsx
+++ conjugator/components/login.jsx
@@ -1,14 +1,62 @@
 import React from "react";
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome Back!</h1>
-      <p></p>
-    </>
-  );
-};
-
-export default Login;
+function Login({ touched, errors }) {
+    return (
+      <Form className="form">
+        <h1>Dar Una Buena Acogida</h1>
+        <div className="form-box">
+          <label className="label">Username</label>
+          <Field className="input"
+          name="username"
+          type="text"
+          autoComplete="off"
+          />
+          <p>{touched.username && errors.username}</p>
+        </div>
+        <div className="form-box">
+          <label className="label">Password</label>
+          <Field
+            className="input"
+            name="password"
+            type="password"
+            autoComplete="off"
+          />
+          <p>{touched.password && errors.password}</p>
+        </div>
+        <button type="submit" className="btn">
+          Submit &rarr;
+        </button>
+      </Form>
+    );
+  }
+       
+  export default withFormik({
+    mapPropsToValues() {
+      return {
+        username: "",
+        password: ""
+      };
+    },
+  
+    validationSchema: Yup.object().shape({
+      username: Yup.string().required("Username is required"),
+      password: Yup.string()
+      .min(6)
+      .required("password is required")
+    }),
+   
+    handleSubmit(values, formikBag) {
+      const url = "https://sp-conjugator-be.herokuapp.com";
+  
+    //   axios 
+    //   .post(url,values)
+    //   .then(res => {
+    //     localStorage.setItem("token", res.data.payload);
+    //     formikBag.props.history.push("");
+    //   })
+    //   .catch(e => {
+    //     console.log(e.response);
+    //   });
+    // }
+  })(Login);
+  
--- conjugator/src/App.js
+++ conjugator/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";
+import FormikReg from 'react';
 import './App.css';
 
+// const ProtectedRoute = ({ component: Component, ...rest }) => {
+//   return <Route {...rest} render={props => {
+//     if (localStorage.getItem('token')) {
+//       return <Component {...props} />;
+//     } else {
+//       return <Redirect to="/" />;
+//     }
+//   }} />;
+// };
+
+// const protectRoute = Component => props => {
+//   if (localStorage.getItem('token')) {
+//     return <Component {...props} />;
+//   } else {
+//     return <Redirect to="/" />;
+//   }
+// };
+
+// const ProtectedBubblePage = protectRoute
+
 function App() {
+  const [colorList, setColorList] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Route exact path="/" component={Login} />
+      <Route path="/bubblepage" component={ProtectedBubblePage}/>
+      </div>
   );
 }
+
+export default App;
 
 export default App;
