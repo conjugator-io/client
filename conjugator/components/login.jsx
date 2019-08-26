7aebe5146d6c527514c3f9a0793e54ff2dc10c96
dangolbeeker <scottb305@outlook.com>
2019-08-26 18:04:07 -0400

    mock login

---
+++ conjugator/components/login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Login = () => {
+  // make a post request to retrieve a token from the api
+  // when you have handled the token, navigate to the BubblePage route
+  return (
+    <>
+      <h1>Welcome Back!</h1>
+      <p></p>
+    </>
+  );
+};
+
+export default Login;
