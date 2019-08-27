4d5b63f62b919700e1e5d449e2588f93cee0c770
dangolbeeker <scottb305@outlook.com>
2019-08-26 20:12:40 -0400

    pull 2

--- conjugator/src/App.js
+++ conjugator/src/App.js
@@ -3,36 +3,16 @@ import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom
 import FormikReg from 'react';
 import './App.css';
 
-// const ProtectedRoute = ({ component: Component, ...rest }) => {
-//   return <Route {...rest} render={props => {
-//     if (localStorage.getItem('token')) {
-//       return <Component {...props} />;
-//     } else {
-//       return <Redirect to="/" />;
-//     }
-//   }} />;
-// };
 
-// const protectRoute = Component => props => {
-//   if (localStorage.getItem('token')) {
-//     return <Component {...props} />;
-//   } else {
-//     return <Redirect to="/" />;
-//   }
-// };
-
-// const ProtectedBubblePage = protectRoute
 
 function App() {
-  const [colorList, setColorList] = useState([]);
+  const [Register, Login] = useState([]);
   return (
       <div className="App">
         <Route exact path="/" component={Login} />
-      <Route path="/bubblepage" component={ProtectedBubblePage}/>
+      <Route path="/Register" component={Register}/>
       </div>
   );
 }
-
-export default App;
 
 export default App;
