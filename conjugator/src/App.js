b20059581083863221caf47d24d44122aee5a856
ampersand <ampersand@ampersands-MacBook-Pro.local>
2019-08-29 12:26:32 -0700

    again

--- conjugator/src/App.js
+++ conjugator/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
-// import Login from './components/Login'
+import Login from './components/Login';
+import Home from './components/Home/Home';
+import Challenge from './components/Challenge';
 // import { protectRoute } from './utils'
 // const ProtectedRoute = protectRoute(Component goes here)
 // import Challenge from './components/Challenge';
 function App() {
   return (
     <div className="App">
-      {/* < Challenge /> *** Michael Still working on this */}
-      {/* <Route to="/login" component={Login} /> ***Noor Still working on this*** */}
+      <Route exact path='/' component={Login} />
+      <Route to='/home' component={Home} />
+      <Route to='/challenge' component ={Challenge}/>
     </div>
   );
 }
