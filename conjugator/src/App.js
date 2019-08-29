544810a311435c404c51e56618d05eefc39e9910
_michelangeloxo <52550247+MADemery@users.noreply.github.com>
2019-08-28 21:10:17 -0700

    Challenge page functioning

--- conjugator/src/App.js
+++ conjugator/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
-import { Route } from 'react-router-dom';
+// import { Route } from 'react-router-dom';
 // import Login from './components/Login'
 // import { protectRoute } from './utils'
 // const ProtectedRoute = protectRoute(Component goes here)
-// import Challenge from './components/Challenge';
+import Challenge from './components/Challenge';
 function App() {
   return (
     <div className="App">
-      {/* < Challenge /> *** Michael Still working on this */}
+      < Challenge /> 
       {/* <Route to="/login" component={Login} /> ***Noor Still working on this*** */}
     </div>
   );
--- conjugator/src/components/Challenge.jsx
+++ conjugator/src/components/Challenge.jsx
@@ -1,43 +1,126 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+// import CardChallenge from './CardChallenge';
+import styled from 'styled-components';
 
+const Styler = styled.div`
+height: 750px;	
+width: 1024px;	
+background-color: #0A4699;
+`;
+
+const StylerP = styled.p`
+height: 198px;	
+width: 750px;	
+color: #FFE981;	
+font-family: Nunito;	
+font-size: 48px;	
+line-height: NaNpx;
+`;
+
+const StylerB = styled.button`
+height: 45px;	
+width: 120px;	
+border-radius: 6px;	
+background-color: #FCE781;
+ 
+    	
+    color: #0A4699;	
+    font-family: Nunito;
+        font-size: 18px;
+        	line-height: 24px;
+`;
+
+const StylerC = styled.a`
+text-decoration: none;
+`;
 
 const Challenge = (props) => {
-    const [challenge, setChallenge] = useState({});
-   
+    const [challenge, setChallenge] = useState([]);
+    const [data, setData] = useState();
+    const [sentence, setSentence] = useState('');
+    const [inputValue, setInputValue] = useState("");
+    const [counter, setCounter] = useState(0);
+    const [error, setError] = useState('');
+
     useEffect(() => {
-    //   const id = props.match.params.id;
-         axios
+      
+
+      if(!data){
+        axios
          
-          .get(`https://sp-conjugator-be.herokuapp.com/api/verbs`)
-          .then(response => {
-              console.log(response)
-            setChallenge(response.data);
-          })
-          .catch(error => {
-            console.error(error);
-          });
-  
-    },[]);
-
-
-
-    if (!challenge) {
-        return <div>Loading  information...</div>;
+        .get(`https://sp-conjugator-be.herokuapp.com/api/verbs`)
+        .then(response => {
+            console.log(response.data)
+          setChallenge(response.data[0].sentence);
+          setData(response.data);
+          setSentence(response.data[0].sentence);
+        })
+        .catch(error => {
+          console.error(error);
+        });
       }
-      //   const { verb, conjugation, tense, form, sentence } = challenge;
-      return (
-<div>
-<input type="text" name="conjugation" placeholder="Type conjugation here" />
-<button type="submit">Submit!</button>
-</div>
-      )
+         
+      if(data){
+        setSentence(data[counter].sentence);
+      }
+          
+  
+    },[counter]);
+    const clickButton = (e) =>{
+      e.preventDefault();
+      if(data[counter].conjugation == inputValue){
+        console.log('matches');
+        setCounter(counter + 1);
+        setError("");
+        
+      } else{
+        console.log('wrong')
+        //put errors here
+        setError("Hint: Verb must be in " + data[counter].tense + " tense");
+      }
+    }
+
+    const changeHandler = e =>{
+      // console.log(e.target.value);
+      setInputValue(e.target.value);
+    }
+if(!sentence){ return null; }
+    return (
+        
+ <Styler> <div className="App">
+          <nav>
+          <h1>Conjugator</h1>
+          </nav>
+          
+
+       <StylerP> 
+        <p>
+         {sentence}
+        </p>
+        
+        </StylerP>      
+    <input type="text" name="conjugation" placeholder="Type conjugation here" onChange={changeHandler} value={inputValue} />
+<StylerB onClick={clickButton}><StylerC>Submit!</StylerC> </StylerB> 
+<p>{(error) ? error : null}</p>
+        </div>
+        </Styler>
+      );
+    }
+  
+
+
+
+
+
+
+
       
     
       
     
 
-}
+
 
     
 
