812caa7509a4bff7e59fc5b44cf5f0be0e31eadc
Noor Al Darraji <077068@yahoo.com>
2019-08-30 08:53:47 -0500

    Fix the Signup typo in App js :hammer:

--- conjugator/src/App.js
+++ conjugator/src/App.js
@@ -5,7 +5,7 @@ import Login from './components/Login'
 import { protectRoute } from './utils'
 import Challenge from './components/Challenge';
 import Home from './components/Home/Home'
-import SignUp from './components/SignUp';
+import FormikSignUp from './components/SignUp';
 
 const ProtectedRoute = protectRoute(Home)
 
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="App">
       <Route exact path='/' component={Login} />
-      <Route exact path='/signup' component={SignUp} />
+      <Route exact path='/signup' component={FormikSignUp} />
       <Route path="/challenge" render={(props) => <Challenge {...props} />} />
       <Route path='/home' component={ProtectedRoute} />
     </div>
